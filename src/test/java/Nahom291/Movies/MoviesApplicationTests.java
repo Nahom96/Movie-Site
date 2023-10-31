915c1cc1d5347efd80018ecdf81b334726e8b37e
Nahom96 <arayanahom475@gmail.com>
2023-10-31 13:25:08 -0400

    Starting with Spring boot

---
+++ src/test/java/Nahom291/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package Nahom291.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

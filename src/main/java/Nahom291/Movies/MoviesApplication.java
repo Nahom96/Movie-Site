23f280f077e1d1536fcb07cf7a90c96d83c817b6
Nahom96 <arayanahom475@gmail.com>
2023-10-31 20:33:52 -0400

    Starting with Spring boot

--- src/main/java/Nahom291/Movies/MoviesApplication.java
+++ src/main/java/Nahom291/Movies/MoviesApplication.java
@@ -3,6 +3,8 @@ package Nahom291.Movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Console;
+
 @SpringBootApplication
 public class MoviesApplication {
 
